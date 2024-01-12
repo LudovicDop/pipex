a8bef6c6f366106490cb911e79338cee986c3885
LudovicDop <ludovicdop@gmail.com>
2024-01-12 17:46:56 +0100

    second commit

--- main.c
+++ main.c
@@ -6,20 +6,21 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:03:41 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/01/12 17:08:40 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/01/12 17:44:09 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(int argc, char** argv)
+int	main(int argc, char **argv)
 {
-    if (argc == 5)
-    {
-        
-    }
-    else
-    {
-        perror("argc");
-    }
+	if (argc == 3)
+	{
+        execv(argv[1], argv);
+        perror("execv");
+	}
+	else
+	{
+		ft_putstr_fd("❌ Too many or not enough arguments ❌\n", 2);
+	}
 }
--- pipex.h
+++ pipex.h
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 17:25:40 by ldoppler          #+#    #+#             */
+/*   Updated: 2024/01/12 17:33:03 by ldoppler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include "./libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 #endif
