c6e574ca724c0573959a5abace6f4b2aa5f8619e
Ludovic Doppler <ldoppler@k2r2p1.42mulhouse.fr>
2024-01-22 09:57:05 +0100

    test branch tmp

--- parent.c
+++ parent.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:38:45 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/01/18 19:53:10 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/01/22 09:55:59 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void parent_process(int *pipefd, t_execve *info_execve)
 {
 	int fd = open(info_execve->file2, O_WRONLY);
+    printf("fd : %d\n", fd);
     close(pipefd[1]); // Close the unused write end
 	dup2(pipefd[0], fd);
     close(pipefd[0]); // Close the read end now that we're done with it
